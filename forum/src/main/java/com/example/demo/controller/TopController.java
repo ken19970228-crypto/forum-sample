149204d7967b1bd26c3923ba5bf4e44f27b1f5b3
kentaro <ken19970228@gmail.com>
2025-09-04 18:23:09 +0900

    feat: 記事ページを追加 feat: トップページに記事一覧表示機能を追加 feat: トップページの記事一覧から記事ページへ遷移する機能を追加

--- forum/src/main/java/com/example/demo/controller/TopController.java
+++ forum/src/main/java/com/example/demo/controller/TopController.java
@@ -1,23 +1,36 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.example.demo.entity.Article;
 import com.example.demo.entity.LoginUser;
+import com.example.demo.service.ArticleService;
 
 @SessionAttributes(types = LoginUser.class)
 @Controller
 public class TopController {
+	private ArticleService articleService;
 	
+	public TopController(ArticleService articleService) {
+		this.articleService = articleService;
+	}
 	@ModelAttribute(value = "loginUser")
 	public LoginUser loginUser() {
 		return new LoginUser();
 	}
 
 	@GetMapping("/")
-	public String showTop(@ModelAttribute("loginUser") LoginUser loginUser) {
+	public String showTop(Model model,
+			@ModelAttribute("loginUser") LoginUser loginUser) {
+		
+		List<Article> articles = articleService.getArticles(5);
+		model.addAttribute("articles", articles);
 		return "top";
 	}
 	
--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -4,4 +4,7 @@ VALUES
 	(2, 'taira@test.com', 'testTaira', 'YWJjZGUxMjM0NQ==', 'user', '2025-08-28 00:00:00');
 
 INSERT IGNORE INTO articles (article_id, user_id, user_nickname, title, text, date)
-VALUES(1, 2, 'testTaira', 'タイトルテスト', '本文テスト', '2025-08-28 17:22:38');
+VALUES
+	(1, 2, 'testTaira', 'タイトルテスト1', '本文テスト1', '2025-08-28 17:22:38'),
+	(2, 2, 'testTaira', 'タイトルテスト2', '本文テスト2', '2025-08-28 18:22:38'),
+	(3, 2, 'testTaira', 'タイトルテスト3', '本文テスト3', '2025-08-28 19:22:38');
